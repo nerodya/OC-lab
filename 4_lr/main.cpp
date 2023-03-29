e8196ebfafd0af290ba7c8960337b286e43c3242
rodya <rdblkv@gmail.com>
2023-03-29 21:00:49 +0300

    init

---
+++ 4_lr/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void printFile(string nameFile)
+{
+}
+
+int main(int argc, char *argv[])
+{
+}
